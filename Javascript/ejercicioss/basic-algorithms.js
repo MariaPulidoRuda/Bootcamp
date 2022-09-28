2ac4641cb76121f4b646fa465119b71b3a6b310a
Maria Pulido ruda <mariapulidoruda@MacBook-Air-de-Maria.local>
2022-09-28 23:06:21 +0200

    Iteration#1

--- Javascript/ejercicioss/basic-algorithms.js
+++ Javascript/ejercicioss/basic-algorithms.js
@@ -30,8 +30,8 @@ let firstName = "John"
 
 
 const toy1 = {name: 'Buss myYear', price: 19};
-const toy2 = {name: 'Rallo mcKing', price: 29};
-const sum = 19 + 29; 
+ const toy2 = {name: 'Rallo mcKing', price: 29};
+ const sum = 19 + 29; 
 console.log(sum)
 
 
@@ -39,12 +39,32 @@ console.log(sum)
 y actualiza la propiedad finalPrice de todos los coches con el valor de su propiedad 
 basePrice más el valor de la variable globalBasePrice.*/
 
-let globalBasePrice = 10000;
+/*let globalBasePrice = 10000;
  globalBasePrice = 25000;
 console.log(globalBasePrice)
 
 const car1 = {name: 'BMW m&m', basePrice: 50000, finalPrice: 60000};
-const car2 = {name: 'Chevrolet Corbina', basePrice: 70000, finalPrice: 80000};
+const car2 = {name: 'Chevrolet Corbina', basePrice: 70000, finalPrice: 80000};*/
 
 
+//Iteration3
 
+const product = 10*5;
+console.log(product)
+
+const quotient = 10/2;
+console.log(quotient)
+
+let a = 15;
+let b = 9;
+let rest = a%b;
+console.log(rest)
+
+
+/*var a = 13;
+var b = 5;
+var quo = Math.floor(a/b);
+var rem = a%b;
+console.log('Quotient = ',quo,'Remainder = ',rem);*/
+
+//1.3 Muestra mediante un console el resto de dividir 15 por 9.
