f43986668b68d0afec6f7cb55e0f0615704d28fc
Maria Pulido ruda <mariapulidoruda@MacBook-Air-de-Maria.local>
2022-09-29 17:37:33 +0200

    iteration1

--- Javascript/ejercicioss/basic-algorithms.js
+++ Javascript/ejercicioss/basic-algorithms.js
@@ -1,14 +1,22 @@
 //Iteration#1 
 
+//1.1 Crea una variable llamada myFavoriteHero, asigna el valor Hulk a ella.
+
 let myFavoriteHero;
  myFavouriteHero = "Hulk"; 
 
-let x;
+ //1.2 Crea una variable llamada x, asigna el valor 50 a ella.
+
  x = 50;
 
+ //1.3 Crea una variable llamada 'h' con el valor 5 y otra 'y' con el valor 10. 
+
+let x;
 let h = 5;
  let y = 10;
 
+ //1.4 Crea una otra variable 'z' y asignale el valor de 'h' + 'y'.
+ 
 let z = h + y; 
 console.log(z)
 
@@ -68,4 +76,50 @@ console.log(i)
 
 
 //Iteraciión 4
+
+/*1.1 Consigue el valor "HULK" del array de avengers y muestralo por consola.
+const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];*/
+
+
+const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
+const avenger = avengers[0];
+console.log(avenger)
+
+//1.2 Cambia el primer elemento de avengers a "IRONMAN"
+
+const superheroe = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
+superheroe.splice(0, 0, 'IRONMAN')
+console.log(superheroe)
+
+//1.3 console numero de elementos en el array usando la propiedad correcta de Array.
+
+const longitud = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
+console.log(longitud.length);
+
+
+//1.4 Añade 2 elementos al array: "Morty" y "Summer". Muestra en consola el último personaje del array.
+
+const rickAndMortyCharacters = ["Rick", "Beth", "Jerry"];
+rickAndMortyCharacters.push('Morty','Summer');
+console.log(rickAndMortyCharacters)
+console.log(rickAndMortyCharacters[4])
+
+//1.5 Elimina el último elemento del array y muestra el primero y el último por consola.
+
+const rickAndMortyCharacter = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"];
+rickAndMortyCharacter.pop();
+console.log(rickAndMortyCharacter)
+console.log(rickAndMortyCharacter[0])
+console.log(rickAndMortyCharacter[rickAndMortyCharacter.length - 1])
+
+
+//1.6 Elimina el segundo elemento del array y muestra el array por consola.
+
+const rickAndMorty = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"];
+
+rickAndMorty.splice(1, 1);
+console.log(rickAndMorty)
+
+
+
 
