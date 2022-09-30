9bd26490c31696ce0f24a93cf2cc8a10a1d89ee8
Maria Pulido ruda <mariapulidoruda@MacBook-Air-de-Maria.local>
2022-09-30 19:32:06 +0200

    iteration1

---
+++ Javascript/ejercicioss/functions/functions.js
@@ -0,0 +1,29 @@
+
+//iteración 1
+//Completa la función que tomando dos números como argumento devuelva el más alto.
+
+
+function sum(numberOne , numberTwo) {
+    if(numberOne > numberTwo) {
+        return numberOne
+  } else {
+    return numberTwo
+  }
+  
+}
+  console.log(sum(8,0))
+  
+
+
+//Iteración #2:
+
+/*Buscar la palabra más larga. 
+Completa la función que tomando un array de strings como argumento devuelva el más largo, en 
+caso de que dos strings tenga la misma longitud deberá devolver el primero.
+
+Puedes usar este array para probar tu función:*/
+
+/*const avengers = ['Hulk', 'Thor', 'IronMan', 'Captain A.', 'Spiderman', 'Captain M.'];
+function findLongestWord(param) {
+  // insert code
+}*/
