f4065fbfa07c4e4d8dbc2b2f3c561620d1090236
Maria Pulido ruda <mariapulidoruda@MacBook-Air-de-Maria.local>
2022-10-03 16:04:47 +0200

    iteration4

--- Javascript/ejercicioss/functions/functions.js
+++ Javascript/ejercicioss/functions/functions.js
@@ -37,3 +37,42 @@ function findLongestWord (param){
     return(superheroe)
 }
 console.log(findLongestWord(avengers));
+
+
+//Iteración #3
+
+/*Calcular una suma puede ser tan simple como iterar sobre un array y sumar cada uno de los elementos.
+Implemente la función denominada sumNumbers que toma un array de números como argumento y devuelve la suma de todos los números de la matriz. */
+
+//Puedes usar este array para probar tu función:
+
+const number = [1, 2, 3, 5, 45, 37, 58];
+
+function sumAll(param){
+    let suma = 0;
+    for(let i = 0; i < number.length; i++){
+        suma = suma + number[i]
+    }
+    return suma;
+}
+console.log(sumAll(number));
+
+//Iteration4
+
+//Calcular un promedio es una tarea extremadamente común. Puedes usar este array para probar tu función:
+
+const number = [12, 21, 38, 5, 45, 37, 6];
+
+function average(param){
+    let sum = 0;
+    let promedio = 0;
+
+    for(let i = 0; i < number.length;i++){
+        sum += number[i]
+    }
+
+    promedio = sum / number.length;
+    return promedio;
+}
+
+console.log(average(number));
