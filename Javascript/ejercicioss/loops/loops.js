68a86c49800d9656be61bedfd65c1215c44cc4fd
Maria Pulido ruda <mariapulidoruda@MacBook-Air-de-Maria.local>
2022-10-05 19:58:37 +0200

    iteration5

--- Javascript/ejercicioss/loops/loops.js
+++ Javascript/ejercicioss/loops/loops.js
@@ -43,3 +43,32 @@ for (let key in alien) {
     console.log(`La clave: ${key}, tiene como valor: ${alien[key]}`);
 }
 
+
+/*Iteración #5: Probando For**
+
+Usa un bucle **for** para recorrer todos los destinos del array y elimina los elementos que tengan el id 11 
+y 40. Imprime en un ***console log*** el array. 
+Puedes usar este array:*/
+
+
+const placesTravel = [{id: 5, name: 'Japan'}, {id: 11, name: 'Venecia'}, {id: 23, name: 'Murcia'}, {id: 40, name: 'Santander'}, {id: 44, name: 'Filipinas'}, {id: 59, name: 'Madagascar'}]
+const newPlaces = []
+for (let i = 0; i < placesTravel.length; i++) {
+if (placesTravel[i].id != 11) {
+    if (placesTravel[i].id != 40) {
+        newPlaces.push(placesTravel[i])
+    }
+    }
+}
+
+console.log(newPlaces)
+
+
+
+/**Iteración #6: 
+
+Mixed For...of e includes**
+
+Usa un bucle **for...of** para recorrer todos los juguetes y elimina los que incluyan la palabra gato. Recuerda que puedes 
+usar la función ***.includes()*** para comprobarlo.Puedes usar este array:*/
+
