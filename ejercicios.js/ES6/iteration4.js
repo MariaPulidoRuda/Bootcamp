261c19597d00b11beea4b179918565b53e23ef67
Maria Pulido ruda <mariapulidoruda@MacBook-Air-de-Maria.local>
2022-10-17 07:48:03 +0200

    iteration4

--- ejercicios.js/ES6/iteration4.js
+++ ejercicios.js/ES6/iteration4.js
@@ -1,5 +1,8 @@
-4.1 Dado el siguiente array, devuelve un array con sus nombres 
-utilizando .map().
+//ITERATION 4
+
+/*4.1 Dado el siguiente array, devuelve un array con sus nombres 
+utilizando .map().*/
+
 const users = [
 	{id: 1, name: 'Abel'},
 	{id:2, name: 'Julia'},
@@ -7,22 +10,52 @@ const users = [
 	{id:4, name: 'Amanda'}
 ];
 
-4.2 Dado el siguiente array, devuelve una lista que contenga los valores 
+const usersName = users.map(user => user.name);
+console.log(usersName)
+
+/*4.2 Dado el siguiente array, devuelve una lista que contenga los valores 
 de la propiedad .name y cambia el nombre a 'Anacleto' en caso de que 
-empiece por 'A'.
-const users = [
+empiece por 'A'.*/
+
+const usersNames = [
 	{id: 1, name: 'Abel'},
 	{id:2, name: 'Julia'},
 	{id:3, name: 'Pedro'},
 	{id:4, name: 'Amanda'}
 ];
 
-4.3 Dado el siguiente array, devuelve una lista que contenga los valores 
+const mapList = users.map((user) => {
+  if (user.name.charAt(0) === "A") {
+    return (user.name = "Anacleto");
+  }
+  return user.name;
+});
+
+console.log(mapList);
+
+/*4.3 Dado el siguiente array, devuelve una lista que contenga los valores 
 de la propiedad .name y añade al valor de .name el string ' (Visitado)' 
-cuando el valor de la propiedad isVisited = true.
-const cities = [
+cuando el valor de la propiedad isVisited = true.*/
+/*const cities = [
 	{isVisited:true, name: 'Tokyo'}, 
 	{isVisited:false, name: 'Madagascar'},
 	{isVisited:true, name: 'Amsterdam'}, 
 	{isVisited:false, name: 'Seul'}
-];
+];*/
+const cities = [
+	{ isVisited: true, name: "Tokyo" },
+	{ isVisited: false, name: "Madagascar" },
+	{ isVisited: true, name: "Amsterdam" },
+	{ isVisited: false, name: "Seul" },
+  ];
+  
+  const citiesArray = cities.map((city) => {
+	if (city.isVisited) {
+	  const { name } = city;
+	  city.name += " (Visitado)";
+	  return name;
+	}
+	return city.name;
+  });
+  
+  console.log(cities, citiesArray);
