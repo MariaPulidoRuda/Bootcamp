bbf4dc05400535375ac0a56b7ad2293490d3d24a
Maria Pulido ruda <mariapulidoruda@MacBook-Air-de-Maria.local>
2022-10-17 08:02:40 +0200

    iteration 5

--- ejercicios.js/ES6/iteration5.js
+++ ejercicios.js/ES6/iteration5.js
@@ -1,23 +1,39 @@
 /*5.1 Dado el siguiente array, utiliza .filter() para generar un nuevo array 
-con los valores que sean mayor que 18
-const ages = [22, 14, 24, 55, 65, 21, 12, 13, 90];
+con los valores que sean mayor que 18*/
 
-5.2 Dado el siguiente array, utiliza .filter() para generar un nuevo array 
-con los valores que sean par.
-const ages = [22, 14, 24, 55, 65, 21, 12, 13, 90];
+const ages1 = [22, 14, 24, 55, 65, 21, 12, 13, 90];
 
-5.3 Dado el siguiente array, utiliza .filter() para generar un nuevo array 
-con los streamers que tengan el gameMorePlayed = 'League of Legends'.
-const streamers = [
+const older = ages1.filter((age) => age > 18);
+    console.log(older)
+
+
+/*5.2 Dado el siguiente array, utiliza .filter() para generar un nuevo array 
+con los valores que sean par.*/
+
+const ages = [22, 14, 24, 55, 65, 21, 12, 13, 90];
+const pairAges = ages.filter((age) => age % 2 === 0);
+console.log(pairAges);
+
+
+/*5.3 Dado el siguiente array, utiliza .filter() para generar un nuevo array 
+con los streamers que tengan el gameMorePlayed = 'League of Legends'.*/
+
+const streamers1 = [
 	{name: 'Rubius', age: 32, gameMorePlayed: 'Minecraft'},
 	{name: 'Ibai', age: 25, gameMorePlayed: 'League of Legends'}, 
 	{name: 'Reven', age: 43, gameMorePlayed: 'League of Legends'},
 	{name: 'AuronPlay', age: 33, gameMorePlayed: 'Among Us'}
 ];
 
-5.4 Dado el siguiente array, utiliza .filter() para generar un nuevo array 
+const lolStreamers = streamers1.filter(
+  (streamer) => streamer.gameMorePlayed === "League of Legends"
+);
+console.log(lolStreamers);
+
+/*5.4 Dado el siguiente array, utiliza .filter() para generar un nuevo array 
 con los streamers que incluyan el caracter 'u' en su propiedad .name. Recomendamos 
-usar la funcion .includes() para la comprobación.
+usar la funcion .includes() para la comprobación.*/
+
 const streamers = [
 	{name: 'Rubius', age: 32, gameMorePlayed: 'Minecraft'},
 	{name: 'Ibai', age: 25, gameMorePlayed: 'League of Legends'},
@@ -25,11 +41,29 @@ const streamers = [
 	{name: 'AuronPlay', age: 33, gameMorePlayed: 'Among Us'}
 ];
 
-5.5 utiliza .filter() para generar un nuevo array con los streamers que incluyan 
+  const streamersWithU = streamers.filter((streamer) =>
+    streamer.name.includes("u")
+  );
+  console.log(streamersWithU);
+  const legendsStreamers = streamers.filter((streamer) => {
+    return streamer.gameMorePlayed.includes("Legends");
+  });
+  
+  for (let streamer of legendsStreamers) {
+    if (streamer.age > 35) {
+      streamer.gameMorePlayed = streamer.gameMorePlayed.toUpperCase();
+    }
+  }
+  
+  console.log(legendsStreamers);
+
+/*5.5 utiliza .filter() para generar un nuevo array con los streamers que incluyan 
 el caracter 'Legends' en su propiedad .gameMorePlayed. Recomendamos usar la funcion 
 .includes() para la comprobación.
 Además, pon el valor de la propiedad .gameMorePlayed a MAYUSCULAS cuando 
 .age sea mayor que 35.
+
+
 
 5.6 Dado el siguiente html y javascript, utiliza .filter() para mostrar por consola 
 los streamers que incluyan la palabra introducida en el input. De esta forma, si 
@@ -44,16 +78,9 @@ const streamers = [
 
 
 
-5.7 Dado el siguiente html y javascript, utiliza .filter() para mostrar por consola 
+5/*.7 Dado el siguiente html y javascript, utiliza .filter() para mostrar por consola 
 los streamers que incluyan la palabra introducida en el input. De esta forma, si 
 introduzco 'Ru' me deberia de mostrar solo el streamer 'Rubius'. Si introduzco 'i', 
 me deberia de mostrar el streamer 'Rubius' e 'Ibai'.
-En este caso, muestra solo los streamers filtrados cuando hagamos click en el button.
-const streamers = [
-	{name: 'Rubius', age: 32, gameMorePlayed: 'Minecraft'},
-	{name: 'Ibai', age: 25, gameMorePlayed: 'League of Legends'},
-	{name: 'Reven', age: 43, gameMorePlayed: 'League of Legends'},
-	{name: 'AuronPlay', age: 33, gameMorePlayed: 'Among Us'}
-];
+En este caso, muestra solo los streamers filtrados cuando hagamos click en el button.*/
 
-*/
